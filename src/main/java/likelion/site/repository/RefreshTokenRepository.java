e529e1f5e211709490778869e96685b3406bbc3d
soozzang <soozzang@inha.edu>
2024-01-22 23:30:12 +0900

    feat: jpa상속 레포지토리

---
+++ src/main/java/likelion/site/repository/RefreshTokenRepository.java
@@ -0,0 +1,13 @@
+package likelion.site.repository;
+
+import likelion.site.domain.RefreshToken;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface RefreshTokenRepository extends JpaRepository<RefreshToken, Long> {
+    Optional<RefreshToken> findByKey(String key);
+    Optional<RefreshToken> findByAccessToken(String access);
+}
