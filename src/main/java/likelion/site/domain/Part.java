c3ef81f42aaa400754bf776e76f8a5d34e357fe4
soozzang <soozzang@inha.edu>
2024-01-29 01:47:52 +0900

    feat: 파트 메서드 추가

--- src/main/java/likelion/site/domain/Part.java
+++ src/main/java/likelion/site/domain/Part.java
@@ -1,5 +1,14 @@
 package likelion.site.domain;
 
 public enum Part {
-    BE, FE, STAFF
+    BE, FE, STAFF;
+
+    public static Part findByName(String name) {
+        for (Part part : Part.values()) {
+            if (part.name().equalsIgnoreCase(name)) {
+                return part;
+            }
+        }
+        return null;
+    }
 }
