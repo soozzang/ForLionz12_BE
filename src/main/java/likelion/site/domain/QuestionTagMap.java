8027ca7d15631110512e526fe0649c2fd9ad4cfe
soozzang <soozzang@inha.edu>
2024-01-31 22:50:11 +0900

    feat: NoArgsConstructor 추가

--- src/main/java/likelion/site/domain/QuestionTagMap.java
+++ src/main/java/likelion/site/domain/QuestionTagMap.java
@@ -3,9 +3,11 @@ package likelion.site.domain;
 import jakarta.persistence.*;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Getter
+@NoArgsConstructor
 public class QuestionTagMap {
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "questiontagmap_id")
