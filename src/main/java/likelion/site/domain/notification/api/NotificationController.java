a878272850697d00e7734b8ffba038552b8e04f0
soozzang <soozzang@inha.edu>
2024-02-03 13:54:41 +0900

    feat: 공지 관련 dto 수정

--- src/main/java/likelion/site/domain/notification/api/NotificationController.java
+++ src/main/java/likelion/site/domain/notification/api/NotificationController.java
@@ -8,12 +8,14 @@ import likelion.site.domain.notification.domain.NotificationPart;
 import likelion.site.domain.member.domain.Part;
 import likelion.site.domain.member.service.MemberService;
 import likelion.site.domain.notification.service.NotificationService;
+import likelion.site.global.ApiResponse;
 import likelion.site.global.util.SecurityUtil;
 import lombok.*;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
 
 @Tag(name = "Notification", description = "공지사항")
@@ -26,7 +28,7 @@ public class NotificationController {
 
     @Operation(summary = "공지사항 생성", description = "STAFF인 사용자만 가능합니다.")
     @PostMapping
-    public ResponseEntity<NotificationResponse> createNotification(@RequestBody NotificationRequest request) {
+    public ApiResponse<NotificationResponse> createNotification(@RequestBody NotificationRequest request) {
         if(memberService.findMemberInfoById(SecurityUtil.getCurrentMemberId()).getPart() == Part.STAFF) {
             NotificationPart notificationPart = NotificationPart.findByName(request.part);
             Notification notification = Notification.builder()
@@ -36,35 +38,52 @@ public class NotificationController {
                     .build();
 
             Long id = notificationService.addNotification(notification);
-            return ResponseEntity.ok().body(new NotificationResponse(id));
+            return ApiResponse.createSuccess(new NotificationResponse(id));
         }
         return null;
     }
 
     @Operation(summary = "id를 통해 특정 공지사항 상세 조회")
     @GetMapping("{id}")
-    public ResponseEntity<NotificationDto> getNotificationDetail(@PathVariable("id") Long id) {
+    public ApiResponse<NotificationDto> getNotificationDetail(@PathVariable("id") Long id) {
         Notification notification = notificationService.findNotificationById(id);
-        return ResponseEntity.ok().body(new NotificationDto(notification));
+        return ApiResponse.createSuccess(new NotificationDto(notification));
     }
 
-    @Operation(summary = "파트 별 공지사항 조회" , description = "partName에는 BE/FE/ALL이 들어갈 수 있습니다.")
+    @Operation(summary = "모든 공지사항 조회")
+    @GetMapping("all")
+    public ApiResponse<List<NotificationDto>> findAllNotifications() {
+        List<Notification> notifications = notificationService.findAllNotifications();
+        List<NotificationDto> notificationDtoList = new ArrayList<>();
+
+        for (Notification notification : notifications) {
+            NotificationDto dto = new NotificationDto(notification);
+            notificationDtoList.add(dto);
+        }
+        return ApiResponse.createSuccess(notificationDtoList);
+    }
+
+    @Operation(summary = "파트 별 공지사항 조회", description = "partName에는 BE/FE/ALL이 들어갈 수 있습니다.")
     @GetMapping("part/{part}")
-    public ResponseEntity<Result> findNotificationByPart(@PathVariable("part") String part) {
+    public ApiResponse<List<NotificationDto>> findNotificationByPart(@PathVariable("part") String part) {
         NotificationPart notificationPart = NotificationPart.findByName(part);
+
+        List<NotificationDto> notificationDtoList = new ArrayList<>();
         List<Notification> notifications = notificationService.findByNotificationPart(notificationPart);
-        List<NotificationDto> collect = notifications.stream()
-                .map(NotificationDto::new)
-                .toList();
-        return ResponseEntity.ok().body(new Result(collect));
+
+        for (Notification notification : notifications) {
+            NotificationDto dto = new NotificationDto(notification);
+            notificationDtoList.add(dto);
+        }
+        return ApiResponse.createSuccess(notificationDtoList);
     }
 
     @Operation(summary = "특정 id의 공지사항 업데이트")
     @PutMapping("{id}")
-    public ResponseEntity<NotificationResponse> updateNotification(@PathVariable("id") Long id, @RequestBody NotificationRequest request) {
+    public ApiResponse<NotificationResponse> updateNotification(@PathVariable("id") Long id, @RequestBody NotificationRequest request) {
         NotificationPart notificationPart = NotificationPart.findByName(request.part);
         notificationService.update(id, request.getTitle(), request.getContent(), notificationPart);
-        return ResponseEntity.ok().body(new NotificationResponse(id));
+        return ApiResponse.createSuccess(new NotificationResponse(id));
     }
 
 //    @DeleteMapping
@@ -99,6 +118,7 @@ public class NotificationController {
         @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
         LocalDateTime createdAt;
 
+        @Builder
         public NotificationDto(Notification notification) {
             this.id = notification.getId();
             this.title = notification.getTitle();
