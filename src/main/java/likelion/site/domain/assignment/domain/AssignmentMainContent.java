88328a195a50feadaf4c6fdf79fa96aa62326630
soozzang <soozzang@inha.edu>
2024-02-03 18:25:50 +0900

    feat: 메인 카테고리 수정

--- src/main/java/likelion/site/domain/assignment/domain/AssignmentMainContent.java
+++ src/main/java/likelion/site/domain/assignment/domain/AssignmentMainContent.java
@@ -1,7 +1,7 @@
 package likelion.site.domain.assignment.domain;
 
 public enum AssignmentMainContent {
-    HTML , REACT , DJANGO , AWS , JS , CSS , Docker, Git;
+    HTML , REACT , DJANGO , AWS , JS , CSS , Docker, Git, PYTHON;
 
     public static AssignmentMainContent findByName(String name) {
         for (AssignmentMainContent assignmentMainContent : AssignmentMainContent.values()) {
