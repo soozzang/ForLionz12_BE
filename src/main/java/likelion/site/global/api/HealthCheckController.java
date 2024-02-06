295514a1b8614e9fc98ba14ea475c4a9e5c159a1
soozzang <soozzang@inha.edu>
2024-02-06 16:53:58 +0900

    refactor: 헬스체크api 패키지 변경

--- src/main/java/likelion/site/global/api/HealthCheckController.java
+++ src/main/java/likelion/site/global/api/HealthCheckController.java
@@ -1,4 +1,4 @@
-package likelion.site.domain.member.domain.controller;
+package likelion.site.global.api;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
