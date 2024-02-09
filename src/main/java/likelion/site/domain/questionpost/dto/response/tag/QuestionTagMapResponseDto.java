e8cd875d23512198f89c3faa0c7df000fdda13b4
soozzang <soozzang@inha.edu>
2024-02-10 00:24:15 +0900

    refactor: 매핑객체 dto 수정

--- src/main/java/likelion/site/domain/questionpost/dto/response/tag/QuestionTagMapResponseDto.java
+++ src/main/java/likelion/site/domain/questionpost/dto/response/tag/QuestionTagMapResponseDto.java
@@ -4,12 +4,10 @@ import likelion.site.domain.questionpost.domain.QuestionTagMap;
 
 public class QuestionTagMapResponseDto {
 
-    Long id;
     Long childTagId;
     Long questionPostId;
 
     public QuestionTagMapResponseDto(QuestionTagMap questionTagMap) {
-        this.id = questionTagMap.getId();
         this.childTagId = questionTagMap.getChildTag().getId();
         this.questionPostId = questionTagMap.getQuestionPost().getId();
     }
