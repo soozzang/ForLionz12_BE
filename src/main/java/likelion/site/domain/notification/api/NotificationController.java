08ea2696a20bde098f3c984da79547c7bc7c0c0f
soozzang <soozzang@inha.edu>
2024-02-10 17:31:45 +0900

    #2 refactor: 공지사항 레이어 역할 분리

--- src/main/java/likelion/site/domain/notification/api/NotificationController.java
+++ src/main/java/likelion/site/domain/notification/api/NotificationController.java
@@ -2,20 +2,18 @@ package likelion.site.domain.notification.api;
 
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import likelion.site.domain.notification.domain.Notification;
-import likelion.site.domain.notification.domain.NotificationPart;
-import likelion.site.domain.member.domain.Part;
 import likelion.site.domain.member.service.MemberService;
-import likelion.site.domain.notification.dto.request.NotificationRequestDto;
-import likelion.site.domain.notification.dto.response.NotificationIdResponseDto;
-import likelion.site.domain.notification.dto.response.NotificationResponseDto;
+import likelion.site.domain.notification.dto.request.NotificationRequest;
+import likelion.site.domain.notification.dto.response.NotificationDetailResponse;
+import likelion.site.domain.notification.dto.response.NotificationIdResponse;
 import likelion.site.domain.notification.service.NotificationService;
 import likelion.site.global.ApiResponse;
 import likelion.site.global.util.SecurityUtil;
 import lombok.*;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @Tag(name = "Notification", description = "공지사항")
@@ -28,61 +26,31 @@ public class NotificationController {
 
     @Operation(summary = "공지사항 생성", description = "STAFF인 사용자만 가능합니다.")
     @PostMapping
-    public ApiResponse<NotificationIdResponseDto> createNotification(@RequestBody NotificationRequestDto request) {
-        if(memberService.findMemberInfoById(SecurityUtil.getCurrentMemberId()).getPart() == Part.STAFF) {
-            NotificationPart notificationPart = NotificationPart.findByName(request.getPart());
-            Notification notification = Notification.builder()
-                    .title(request.getTitle())
-                    .notificationPart(notificationPart)
-                    .content(request.getContent())
-                    .build();
-
-            Long id = notificationService.addNotification(notification);
-            return ApiResponse.createSuccess(new NotificationIdResponseDto(id));
-        }
-        return null;
+    public ResponseEntity<ApiResponse<NotificationDetailResponse>> createNotification(@RequestBody NotificationRequest request) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(ApiResponse.createSuccess(notificationService.addNotification(request,SecurityUtil.getCurrentMemberId())));
     }
 
     @Operation(summary = "id를 통해 특정 공지사항 상세 조회")
     @GetMapping("{id}")
-    public ApiResponse<NotificationResponseDto> getNotificationDetail(@PathVariable("id") Long id) {
-        Notification notification = notificationService.findNotificationById(id);
-        return ApiResponse.createSuccess(new NotificationResponseDto(notification));
+    public ResponseEntity<ApiResponse<NotificationIdResponse>> getNotificationDetail(@PathVariable("id") Long id) {
+        return ResponseEntity.ok().body(ApiResponse.createSuccess(notificationService.findNotificationById(id)));
     }
 
     @Operation(summary = "모든 공지사항 조회")
     @GetMapping("all")
-    public ApiResponse<List<NotificationResponseDto>> findAllNotifications() {
-        List<Notification> notifications = notificationService.findAllNotifications();
-        List<NotificationResponseDto> notificationDtoList = new ArrayList<>();
-
-        for (Notification notification : notifications) {
-            NotificationResponseDto dto = new NotificationResponseDto(notification);
-            notificationDtoList.add(dto);
-        }
-        return ApiResponse.createSuccess(notificationDtoList);
+    public ResponseEntity<ApiResponse<List<NotificationIdResponse>>> findAllNotifications() {
+        return ResponseEntity.ok().body(ApiResponse.createSuccess(notificationService.findAllNotifications()));
     }
 
     @Operation(summary = "파트 별 공지사항 조회", description = "partName에는 BE/FE/ALL이 들어갈 수 있습니다.")
     @GetMapping("part/{part}")
-    public ApiResponse<List<NotificationResponseDto>> findNotificationByPart(@PathVariable("part") String part) {
-        NotificationPart notificationPart = NotificationPart.findByName(part);
-
-        List<NotificationResponseDto> notificationDtoList = new ArrayList<>();
-        List<Notification> notifications = notificationService.findByNotificationPart(notificationPart);
-
-        for (Notification notification : notifications) {
-            NotificationResponseDto dto = new NotificationResponseDto(notification);
-            notificationDtoList.add(dto);
-        }
-        return ApiResponse.createSuccess(notificationDtoList);
+    public ApiResponse<List<NotificationIdResponse>> findNotificationByPart(@PathVariable("part") String part) {
+        return ApiResponse.createSuccess(notificationService.findByNotificationPart(part));
     }
 
     @Operation(summary = "특정 id의 공지사항 업데이트")
     @PutMapping("{id}")
-    public ApiResponse<NotificationIdResponseDto> updateNotification(@PathVariable("id") Long id, @RequestBody NotificationRequestDto request) {
-        NotificationPart notificationPart = NotificationPart.findByName(request.getPart());
-        notificationService.update(id, request.getTitle(), request.getContent(), notificationPart);
-        return ApiResponse.createSuccess(new NotificationIdResponseDto(id));
+    public ResponseEntity<ApiResponse<NotificationDetailResponse>> updateNotification(@PathVariable("id") Long id, @RequestBody NotificationRequest request) {
+        return ResponseEntity.ok().body(ApiResponse.createSuccess(notificationService.update(id, request)));
     }
 }
--- src/main/java/likelion/site/domain/notification/service/NotificationService.java
+++ src/main/java/likelion/site/domain/notification/service/NotificationService.java
@@ -1,8 +1,14 @@
 package likelion.site.domain.notification.service;
 
+import likelion.site.domain.member.repository.MemberRepository;
 import likelion.site.domain.notification.domain.Notification;
 import likelion.site.domain.notification.domain.NotificationPart;
+import likelion.site.domain.notification.dto.request.NotificationRequest;
+import likelion.site.domain.notification.dto.response.NotificationDetailResponse;
+import likelion.site.domain.notification.dto.response.NotificationIdResponse;
 import likelion.site.domain.notification.repository.NotificationRepository;
+import likelion.site.global.exception.CustomError;
+import likelion.site.global.exception.exceptions.AuthorizationException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -15,29 +21,38 @@ import java.util.List;
 public class NotificationService {
 
     private final NotificationRepository notificationRepository;
+    private final MemberRepository memberRepository;
 
     @Transactional
-    public Long addNotification(Notification notification){
-        notificationRepository.save(notification);
-        return notification.getId();
+    public NotificationDetailResponse addNotification(NotificationRequest request, Long memberId){
+        if (memberRepository.findById(memberId).get().getPart().isStaff()){
+            Notification notification = request.toEntity();
+            notificationRepository.save(notification);
+            return new NotificationDetailResponse(notification);
+        }
+        throw new AuthorizationException(CustomError.AUTHORIZATION_EXCEPTION);
     }
 
-    public Notification findNotificationById(Long notificationId) {
-        return notificationRepository.findById(notificationId).get();
+    public NotificationIdResponse findNotificationById(Long notificationId) {
+        Notification notification = notificationRepository.findById(notificationId).get();
+        return new NotificationIdResponse(notification);
     }
 
-    public List<Notification> findAllNotifications() {
-        return notificationRepository.findAll();
+    public List<NotificationIdResponse> findAllNotifications() {
+        return NotificationIdResponse.to(notificationRepository.findAll());
     }
 
-    public List<Notification> findByNotificationPart(NotificationPart notificationPart) {
-        return notificationRepository.findByNotificationPart(notificationPart);
+    public List<NotificationIdResponse> findByNotificationPart(String partName) {
+        NotificationPart notificationPart = NotificationPart.findByName(partName);
+        return NotificationIdResponse.to(notificationRepository.findByNotificationPart(notificationPart));
     }
 
     @Transactional
-    public void update(Long id, String title, String content, NotificationPart notificationPart) {
+    public NotificationDetailResponse update(Long id, NotificationRequest request) {
+        NotificationPart notificationPart = NotificationPart.findByName(request.getPart());
         Notification notification = notificationRepository.findById(id).get();
-        notification.updateNotification(title, content, notificationPart);
+        notification.updateNotification(request.getTitle(), request.getContent(), notificationPart);
+        return new NotificationDetailResponse(notification);
     }
 
     @Transactional
