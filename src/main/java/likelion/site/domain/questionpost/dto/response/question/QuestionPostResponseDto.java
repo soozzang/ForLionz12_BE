5e365fe64edf5e320b954f116699e8e7f1290c5e
soozzang <soozzang@inha.edu>
2024-02-11 17:00:33 +0900

    #5 feat: 질문글 응답 dto 수정

--- src/main/java/likelion/site/domain/questionpost/dto/response/question/QuestionPostResponseDto.java
+++ src/main/java/likelion/site/domain/questionpost/dto/response/question/QuestionPostResponseDto.java
@@ -12,14 +12,18 @@ public class QuestionPostResponseDto {
 
     Long id;
     Long memberId;
+    String name;
     String title;
     String content;
+    String memberImageUrl;
     LocalDateTime createdAt;
 
     public QuestionPostResponseDto(QuestionPost questionPost) {
         id = questionPost.getId();
         memberId = questionPost.getMember().getId();
         title = questionPost.getTitle();
+        name = questionPost.getMember().getName();
+        memberImageUrl = questionPost.getMember().getImageUrl();
         content = questionPost.getContent();
         createdAt = questionPost.getCreatedAt();
     }
