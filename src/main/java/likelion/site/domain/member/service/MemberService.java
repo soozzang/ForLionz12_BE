ac434b001d23a8773699262690c3949627db0dcf
soozzang <soozzang@inha.edu>
2024-03-10 17:05:08 +0900

    chore: 테스트

--- src/main/java/likelion/site/domain/member/service/MemberService.java
+++ src/main/java/likelion/site/domain/member/service/MemberService.java
@@ -57,7 +57,7 @@ public class MemberService {
     @Transactional
     public MemberIdResponseDto updateIntroduction(Long id, IntroductionUpdateRequest request) {
         Member member = memberRepository.findById(id).get();
-        member.updateIntroduction(request.getIntroduction());
+        member.updateIntroduction("ㅎㅇ");
         memberRepository.save(member);
         return new MemberIdResponseDto(member);
     }
